16eaadf90787c3de33426c65cae4582a24890b58
Zihao Du <duz12@mcmaster.ca>
2021-02-19 17:25:39 -0500

    fix powerup bugs

--- src/Model/Dino.py
+++ src/Model/Dino.py
@@ -22,17 +22,31 @@ class Dino(pygame.sprite.Sprite):
     def duck(self):
         if self.isJumping == False:
             self.isDucking = True
-            self.image = pygame.transform.scale(pygame.image.load('../images/Dino.png'), (85, 35))
-            self.rect = self.image.get_rect()
-            self.rect.bottom = self.screen_rect.bottom - 40
-            self.rect.left = self.screen_rect.left + 100
+            if self.isInvincible == False:
+                self.image = pygame.transform.scale(pygame.image.load('../images/Dino.png'), (85, 35))
+                self.rect = self.image.get_rect()
+                self.rect.bottom = self.screen_rect.bottom - 40
+                self.rect.left = self.screen_rect.left + 100
+            else: 
+                self.image = pygame.transform.scale(pygame.image.load('../images/DinoInvincible.jpg'), (85, 35))
+                self.rect = self.image.get_rect()
+                self.rect.bottom = self.screen_rect.bottom - 40
+                self.rect.left = self.screen_rect.left + 100
+
     def stand(self):
         if self.isJumping == False:
             self.isDucking = False
-            self.image = pygame.transform.scale(pygame.image.load('../images/Dino.png'), (75, 75))
-            self.rect = self.image.get_rect()
-            self.rect.bottom = self.screen_rect.bottom - 40
-            self.rect.left = self.screen_rect.left + 100
+            if self.isInvincible == False:
+                self.image = pygame.transform.scale(pygame.image.load('../images/Dino.png'), (75, 75))
+                self.rect = self.image.get_rect()
+                self.rect.bottom = self.screen_rect.bottom - 40
+                self.rect.left = self.screen_rect.left + 100
+            else:
+                self.image = pygame.transform.scale(pygame.image.load('../images/DinoInvincible.jpg'), (75, 75))
+                self.rect = self.image.get_rect()
+                self.rect.bottom = self.screen_rect.bottom - 40
+                self.rect.left = self.screen_rect.left + 100
+
     def jump(self):
         if self.isJumping == False and self.isDucking == False:
             self.isJumping = True
@@ -57,9 +71,15 @@ class Dino(pygame.sprite.Sprite):
             self.movement[1] += 1
         self.rect = self.rect.move(self.movement)   
         self.checkbounds()
-        if self.start_time + 5000 < pygame.time.get_ticks() and self.isInvincible == True:
+        if self.start_time + 5000 < pygame.time.get_ticks() and self.isInvincible == True and self.isDucking == False:
             self.isInvincible = False
             self.image = pygame.transform.scale(pygame.image.load('../images/Dino.png'), (75, 75))
+            self.rect = self.image.get_rect()
+            self.rect.left = self.screen_rect.left + 100
+            self.rect.bottom = self.screen_rect.bottom - 40
+        elif self.start_time + 5000 < pygame.time.get_ticks() and self.isInvincible == True and self.isDucking == True:
+            self.isInvincible = False
+            self.image = pygame.transform.scale(pygame.image.load('../images/Dino.png'), (85, 35))
             self.rect = self.image.get_rect()
             self.rect.left = self.screen_rect.left + 100
             self.rect.bottom = self.screen_rect.bottom - 40
