92031b4c346afdf279650599d7387a9fa08e950a
Andrew Balmakund <balmakua@mcmaster.ca>
2021-03-20 16:17:23 -0400

    Moved to prototype folder

---
+++ src/Prototype/Model/Obstacle.py
@@ -0,0 +1,56 @@
+import pygame
+class Obstacle(pygame.sprite.Sprite):
+    # name: name of obstacle
+    # width: width of image of obstacle
+    # height: height of image of obstacle
+    # imgPath: image path for obstacle
+    def __init__(self, name, width, height, speed, imgPath):
+        super().__init__()
+        self.__name = name
+        self.__width = width
+        self.__height = height
+        self.__speed = speed
+        self.__img = pygame.image.load(imgPath)
+        self.rect = self.__img.get_rect()
+
+    def getWidth(self):
+        return self.__width
+
+    def setWidth(self, width):
+        if (width < 0):
+            print("Invalid Width")
+        self.__width = width
+    
+    def getHeight(self):
+        return self.__height
+   
+    def setHeight(self, height):
+        if (height < 0):
+            print("Invalid Height")
+        self.__height = height
+    
+    def getSpeed(self):
+        return self.__speed
+
+    def setSpeed(self, newSpeed):
+        if (newSpeed < 0):
+            print("Invalid Speed: Should be greater than 0")
+        self.__speed = newSpeed
+
+    def getImg(self):
+        return self.__img
+
+    def setImg(self, img):
+        try:  
+            self.__img = img
+            self.rect = self.__img.get_rect()      
+        except FileNotFoundError:
+            print("Wrong file or file path")
+
+    def getRect(self):
+        return self.rect
+
+    def setRect(self, x, y):
+        self.rect.left = x
+        self.rect.bottom = y + self.getHeight() 
+
