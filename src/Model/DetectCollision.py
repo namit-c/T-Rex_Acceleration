313f92e5bd364d272ab0ed43005e922a5a741b63
Andrew Balmakund <balmakua@mcmaster.ca>
2021-03-20 16:18:47 -0400

    deleted files, moved to prototype folder

--- src/Model/DetectCollision.py
+++
@@ -1,6 +0,0 @@
-import pygame
-def DetectCollision(character, obstacle):
-    #print(character.rect, obstacle.rect, obstacle.getWidth(), obstacle.getHeight())
-
-    return character.rect.colliderect(obstacle.getRect()) 
-
