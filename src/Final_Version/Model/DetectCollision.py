d2950c3567ed248147393c31d5cc86aec7e95425
Andrew Balmakund <balmakua@mcmaster.ca>
2021-03-20 17:54:55 -0400

    Added doxygen comments to DetectCollision

--- src/Final_Version/Model/DetectCollision.py
+++ src/Final_Version/Model/DetectCollision.py
@@ -1,11 +1,31 @@
+"""@package docstring
+Documentation for this module.
+ 
+Obstacle class
+Author: Dev^(enthusiases)
+This class is responsible for maintaining several properties of the obstacle object
+"""
+
 import pygame
 
+##
+# @file DetectCollision.py
+# @brief Reponsible for checking collisions between a character and other elements (i.e powerups, obstacles) 
+
+
+## @brief Determine if a character has collided with an element
+#  @param character pygame.sprite 
+#  @param element pygame.sprite
+#  @return return a boolean where if the character and element have collided then return True, otherwise return False 
 def detect_collision(character, element): 
     return character.get_rect().colliderect(element.get_rect())
 
+## @brief Determine which element the character has collided with
+#  @param character pygame.sprite
+#  @param element_list list of pygame.sprite
+#  @return return the specific element that first collides with the character, if there is no collisio then return None
 def find_collision(character, element_list):
     for element in element_list:
         if (detect_collision(character, element)):
             return element
-    
     return None
