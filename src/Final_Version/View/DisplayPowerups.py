f35e6a83727743e1c1d13a2a164f25b8373a9be4
Zihao Du <duz12@mcmaster.ca>
2021-03-20 18:56:37 -0400

    Add random powerups

--- src/Final_Version/View/DisplayPowerups.py
+++ src/Final_Version/View/DisplayPowerups.py
@@ -1,3 +1,4 @@
+import random
 sys.path.insert(1, '../Model')
 from Powerups import *
 
@@ -7,9 +8,12 @@ class DisplayPowerups():
             raise Exception("IllegalArguemntException")
         self.game_screen = game_screen
         self.powerups_displayed = []
+        self.__generate_time = time.time()
     def generate_powerups(self, speed):
-        new_powerups = Powerups(self.game_screen, 50, 50, speed)
-        self.powerups_displayed.append(new_powerups)
+        if (self.__generate_time >= time.time() + randint(3,5))):
+            new_powerups = Powerups(self.game_screen, 50, 50, speed)
+            self.powerups_displayed.append(new_powerups)
+            self.__generate_time = time.time()
     def draw_powerups(self, powerups):
         self.game_screen.blit(powerups.get_image(), powerups.get_image().get_rect())
     def update_powerups(self):
