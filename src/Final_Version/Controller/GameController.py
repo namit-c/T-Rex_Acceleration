07632afc042c1f3fe866eb8a7c1c355f845fc19d
Andrew Balmakund <balmakua@mcmaster.ca>
2021-03-20 23:19:42 -0400

    Added game controller (drawing background and draw floor), fixed several errors with function naming

--- src/Final_Version/Controller/GameController.py
+++ src/Final_Version/Controller/GameController.py
@@ -40,14 +40,16 @@ class GameController():
         self.__obstacle_list = []
         self.__powerup_list = []
         
-        self.__character = Character(self.__game_screen, assets.load_character()[0])
+        self.__load_character = assets.load_character()
+        self.__character = Character.Character(self.__game_screen, self.__load_character[0])
+        
         # background music 
 
         #####---------------
         self.__floor = assets.load_floor()
         self.__floor_position = 0
         self.__background = assets.load_background()
-        self.__score_count = Score()
+        self.__score_count = Score.Score()
 
     ## Method that checks the user input
     def check_user_input(self):
@@ -58,7 +60,7 @@ class GameController():
                 if event.key == pygame.K_DOWN:
                     self.__character.duck()
                 if event.key == pygame.K_UP:
-                    self.__character.jump()
+                    self.__character.jump(self.__load_character[2],self.__load_character[1])
                 if event.key == pygame.K_p:
                     MenuController.pause_menu()
             elif event.type == pygame.KEYUP:
@@ -74,10 +76,10 @@ class GameController():
         bg_rgb = [153, 255, 255]
 
         # Defining Variables for the view methods
-        display_obstacles = DisplayObstacle(self.__game_screen)
-        display_character = DisplayCharacter(self.__game_screen, self.__character)
-        display_environment = DisplayEnvironment(self.__game_screen)
-        display_powerups = DisplayPowerups(self.__game_screen)
+        display_obstacles = DisplayObstacle.DisplayObstacle(self.__game_screen)
+        display_character = DisplayCharacter.DisplayCharacter(self.__game_screen, self.__character)
+        display_environment = DisplayEnvironment.DisplayEnvironment(self.__game_screen)
+        display_powerups = DisplayPowerups.DisplayPowerups(self.__game_screen)
         instructions = "To play: Jump is Up Arrow, Duck is Down Arrow"
         
         # The game loop
@@ -85,11 +87,14 @@ class GameController():
             clock.tick(45)
 
             # Drawing environment elements
-            display_environment.draw_background(self.__background, self.__game_screen)
+            display_environment.draw_background(self.__background, bg_rgb)
             display_environment.draw_floor(self.__floor, self.__floor_position)
             if(time() - self.__score_count.get_start_time() <= 5):
                 display_environment.draw_instruction(instructions) 
             display_environment.draw_score(self.__score_count)
+
+            self.check_user_input()
+            pygame.display.update()
 
 game = GameController()
 game.game_loop()
