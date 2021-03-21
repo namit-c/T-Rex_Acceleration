ac2ec56960d30256a03169334a7f13f9d8215c0e
Namit Chopra <choprn9@mcmaster.ca>
2021-03-20 21:48:31 -0400

    made a display menu module

---
+++ src/Final_Version/View/DisplayMenu.py
@@ -0,0 +1,7 @@
+"""@package docstring
+Documentation for this module.
+ 
+Display Menu class
+Author: Dev^(enthusiases)
+This class is responsible for displaying different menus on the game screen.
+"""
