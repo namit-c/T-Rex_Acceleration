1f8ed3b182f6d7ffb175ef1904e11def8419d7a4
Namit Chopra <choprn9@mcmaster.ca>
2021-03-20 21:45:59 -0400

    made a play sound module

---
+++ src/Final_Version/View/PlaySound.py
@@ -0,0 +1,8 @@
+"""@package docstring
+Documentation for this module.
+ 
+Play Sound class
+Author: Dev^(enthusiases)
+This class is responsible for playing the background music and different
+sound effects.
+"""
