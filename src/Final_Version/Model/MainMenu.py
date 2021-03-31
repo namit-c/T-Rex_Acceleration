dbf36d866dadfd116c88b90f8587d62d7f938d0b
Namit Chopra <choprn9@mcmaster.ca>
2021-03-30 20:24:46 -0400

    doxygen comments for main menu

--- src/Final_Version/Model/MainMenu.py
+++ src/Final_Version/Model/MainMenu.py
@@ -6,18 +6,22 @@ Author: Dev^(enthusiases)
 This class is responsible for tracking the different game sound volumes. 
 """
 
-## This is a class used to store the different game sound volumes
+##
+# @file MainMenu.py
+# @brief This is a class used to store the different game sound volumes
+
+
 def MainMenu():
 
     ## Defining constant for the maximum volumes
     MAX_VOLUME = 100
 
-    ## Constructor that initializes different sound volumes
+    ## @brief Constructor that initializes different sound volumes
     def __init__(self):
         self.__background_music_volume = MAX_VOLUME
         self.__sound_effects_volume = MAX_VOLUME
 
-    ## Method that updates the volume of the game sounds
+    ## @brief Method that updates the volume of the game sounds
     # @param new_background_volume : an integer that represents the new value of 
     # the background music volume
     # @param new_sound_effects_volume : an integer that represents the new value
@@ -26,7 +30,7 @@ def MainMenu():
         self.__background_music_volume = new_background_volume
         self.__sound_effects_volume = new_sound_effects_volume
     
-    ## Method that returns the current volume levels of the sounds
+    ## @brief Method that returns the current volume levels of the sounds
     # @return the volume of the background music and sound effects
     def get_volumes():
         return self.__background_music_volume, self.__sound_effects_volume
