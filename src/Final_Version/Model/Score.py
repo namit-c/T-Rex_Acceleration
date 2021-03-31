5cf19f07c4aabf501bd02d616e3476c866eed8e3
Namit Chopra <choprn9@mcmaster.ca>
2021-03-30 20:08:28 -0400

    doxygen comments score.py

--- src/Final_Version/Model/Score.py
+++ src/Final_Version/Model/Score.py
@@ -11,7 +11,7 @@ from time import time
 ## This is a class used to keep track of the player's score
 class Score():
 
-    # @brief Constructor that initializes different fields required to track the 
+    ## @brief Constructor that initializes different fields required to track the 
     # player score. The start time is set to the time the constructor is called,
     # which also the time the game starts. The other parameters about the score 
     # (high score, current score, and previous score) are set to 0.
@@ -22,7 +22,7 @@ class Score():
         self.__SCALE_FACTOR = 5
         self.__BOOST = 0
 
-    # @brief Method that updates the current score
+    ## @brief Method that updates the current score
     # @param start_time the time the current game starts
     # @return the current and previous score after the score has been updated
     def update_score(self, start_time):
@@ -37,21 +37,21 @@ class Score():
 
         return self.__current_score, self.__previous_score
 
-    # @brief Method to retrieve the high score of the current game session
+    ## @brief Method to retrieve the high score of the current game session
     # @return the high score of the current game session
     def get_score(self):
         return self.__high_score
     
-    # @brief Method to retrieve the current score of the game
+    ## @brief Method to retrieve the current score of the game
     # @return the current score of the game
     def get_current_score(self):
         return self.__current_score
 
-    # @brief Method to change the current score and score boost to 0 for the next game
+    ## @brief Method to change the current score and score boost to 0 for the next game
     def reset_score(self):
         self.__current_score = 0
         self.__BOOST = 0
 
-    # @brief Method to indicate the score boost powerup is acquired
+    ## @brief Method to indicate the score boost powerup is acquired
     def boost(self):
         self.__BOOST += 1
