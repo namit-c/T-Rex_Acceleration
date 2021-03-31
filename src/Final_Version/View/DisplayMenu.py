90030785346844ce7beb93d14970f0a362155749
Andrew Balmakund <balmakua@mcmaster.ca>
2021-03-30 20:57:55 -0400

    addex doxygen comments

--- src/Final_Version/View/DisplayMenu.py
+++ src/Final_Version/View/DisplayMenu.py
@@ -13,35 +13,39 @@ import time
 ## This is a class used to control the flow at different menus
 class DisplayMenu():
 
-    ## Constructor used to initialize the screen where the menus will be drawn 
-    # @param window : the game screen, a pygame.display object, where elements are drawn
+    ## @brief Constructor used to initialize the screen where the menus will be drawn 
+    # @param window the game screen, a pygame.display object, where elements are drawn
     def __init__(self, window):
         self.__game_screen = window
 
-    ## Method used to display the main menu on the screen
-    # @param main_menu_img : the image of the main menu
+    ## @brief Method used to display the main menu on the screen
+    # @param main_menu_img  the image of the main menu
     def display_main_menu(self, main_menu_img):
         self.__game_screen.blit(main_menu_img, (0,0))
 
-    ## Method used to display the pause menu on the screen
-    # @param main_menu_img : the image of the pause menu
+    ## @brief Method used to display the pause menu on the screen
+    # @param main_menu_img the image of the pause menu
     def display_pause_menu(self, pause_menu_img):
         self.__game_screen.blit(pause_menu_img, (0,0))
 
-    ## Method used to display the end menu on the screen
-    # @param end_menu_img : the image of the end menu
+    ## @brief Method used to display the end menu on the screen
+    #  @param current_score current score the user has achieved
+    #  @param high_score highest score the user has achieved since the start of the game running 
+    #  @param end_menu_img the image of the end menu
     def display_end_menu(self, current_score, high_score, end_menu_img):
         self.__game_screen.blit(end_menu_img, (0,0))
         self.display_msg(str(current_score), (350, 220))
         self.display_msg(str(high_score), (350, 350))
     
-    ## Method used to display the instructions menu on the screen
-    # @param instruction_menu_img : the image of the instructions menu
+    ## @brief Method used to display the instructions menu on the screen
+    #  @param instruction_menu_img the image of the instructions menu
     def display_instruction_menu(self, instruction_menu_img):
         self.__game_screen.blit(instruction_menu_img, (0,0))
 
-    ## Method used to display the setting menu on the screen
-    # @param setting_menu_img : the image of the main menu
+    ## @brief Method used to display the setting menu on the screen, handle button events for user increasing and decreasing the volume setting for background and sound effect
+    #  @param initial_sound_effect inital sound effect volume setting when user joins the settings menu
+    #  @param initial_background intial background volume setting when user joins the settings menu
+    #  @param setting_menu_img the image of the setting menu
     def display_setting_menu(self, play_sound, initial_sound_effect, initial_background, setting_menu_img):
         self.__game_screen.blit(setting_menu_img, (0,0))
         font = pygame.font.SysFont('arial', 100) 
@@ -98,7 +102,10 @@ class DisplayMenu():
         
         return current_sound, current_background
 
-    # for the button on the main menu
+    ## @brief General button placement for displaying two buttons - the plus and minus buttons. Handles event if one specific button has been pressed
+    #  @param font pygame.font for displaying the plus and minus symbols
+    #  @param button1_pos list of two elements x and y value for the first button which is the minus button
+    #  @param button2_pos list of two elements x and y value for the second button which is the plus button 
     def buttons_setting(self, font, button1_pos, button2_pos):
         mouse_position = pygame.mouse.get_pos()
         mouse_clicked = pygame.mouse.get_pressed()
@@ -120,10 +127,14 @@ class DisplayMenu():
         return None
    
 
+    ## @brief displaying the amount of time left till the game resume back
+    #  @param time current time remaining
     def display_resume_menu(self, time):
         self.display_msg("Resuming Back in " + str(time), (250, 250)) 
 
-
+    ## @brief display a specific string message at a specific location
+    #  @param msg string of message to be displayed
+    #  @param msg_pos tuple of (x,y) for the message to display the message
     def display_msg(self, msg, msg_pos):
        	pygame.font.init() # you have to call this at the start, 
         my_font = pygame.font.Font("../assets/Acceleration-Reaction.ttf", 50)
