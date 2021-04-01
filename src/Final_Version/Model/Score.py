55dfe25d71c5d6be05f9c63b57c9240bdcacec91
Namit Chopra <choprn9@mcmaster.ca>
2021-04-01 19:45:05 -0400

    moved method and changed variable names

--- src/Final_Version/Model/Score.py
+++ src/Final_Version/Model/Score.py
@@ -21,12 +21,17 @@ class Score():
         self.__high_score = 0
         self.__current_score = 0
         self.__previous_score = 0
-        
+        self.__score_boost = 0
+
         # Constant for the score boost
         self.__SCORE_BOOST_VAL = 100
 
         self.__SCALE_FACTOR = 5
-        self.__BOOST = 0
+
+    ## @brief Method to retrieve the current score of the game
+    # @return the current score of the game
+    def get_current_score(self):
+        return self.__current_score
 
     ## @brief Method that updates the current score
     # @param start_time the time the current game starts
@@ -35,7 +40,7 @@ class Score():
         self.__previous_score = self.__current_score
         
         # Updating the current score based on current time and scale factor
-        self.__current_score = round((time()-start_time) * self.__SCALE_FACTOR  + self.__BOOST*self.__SCORE_BOOST_VAL)
+        self.__current_score = round((time()-start_time) * self.__SCALE_FACTOR  + self.__score_boost*self.__SCORE_BOOST_VAL)
 
         # Updating the high score if the current score is greater
         if self.__current_score > self.__high_score:
@@ -47,17 +52,12 @@ class Score():
     # @return the high score of the current game session
     def get_score(self):
         return self.__high_score
-    
-    ## @brief Method to retrieve the current score of the game
-    # @return the current score of the game
-    def get_current_score(self):
-        return self.__current_score
 
     ## @brief Method to change the current score and score boost to 0 for the next game
     def reset_score(self):
         self.__current_score = 0
-        self.__BOOST = 0
+        self.__score_boost = 0
 
     ## @brief Method to indicate the score boost powerup is acquired
     def boost(self):
-        self.__BOOST += 1
+        self.__score_boost += 1
