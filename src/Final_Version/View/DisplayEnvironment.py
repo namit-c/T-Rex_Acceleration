388981c3e5be447e3ee3cadbbd02bbb7daf9c01a
Namit Chopra <choprn9@mcmaster.ca>
2021-04-01 20:23:33 -0400

    moved methods

--- src/Final_Version/View/DisplayEnvironment.py
+++ src/Final_Version/View/DisplayEnvironment.py
@@ -33,33 +33,35 @@ class DisplayEnvironment():
     ## @brief Constructor the intitalize the game screen
     # @param window the pygame screen that elements will be drawn on
     def __init__(self, window):
+        if window is None:
+            raise Exception("IllegalArgumentException")
         self.__game_screen = window
-
-    ## @brief Displaying a message(string) to draw on screen 
-    #  @param msg String
-    #  @param msg_pos a tuple of (x,y) position to be drawn on screen
-    def display_msg(self, msg, msg_pos):
-       	pygame.font.init() # you have to call this at the start, 
-        my_font = pygame.font.Font('../assets/Sangharia Demo.ttf', DisplayEnvironment.FONTSIZE)
-        text_surface = my_font.render(msg, True, DisplayEnvironment.WHITE)
-        self.__game_screen.blit(text_surface,msg_pos)
 
     ## @brief Drawing the score onto the screen
     #  @param score Score object
     def draw_score(self, score, clock):
         self.display_msg("Score: " + str(score), DisplayEnvironment.SCORE_POS) 
         self.display_msg("FPS: " + str(int(clock.get_fps())), DisplayEnvironment.FPS_POS) 
+
+    ## @brief Displaying a message(string) to draw on screen 
+    #  @param msg the string containing the message
+    #  @param msg_pos a tuple of (x,y) position to be drawn on screen
+    def display_msg(self, msg, msg_pos):
+       	pygame.font.init() # you have to call this at the start, 
+        my_font = pygame.font.Font('../assets/Sangharia Demo.ttf', DisplayEnvironment.FONTSIZE)
+        text_surface = my_font.render(msg, True, DisplayEnvironment.WHITE)
+        self.__game_screen.blit(text_surface,msg_pos)
     
     ## @brief Drawing the floor image on the game screen
     #  @param floor the image of the floor to be drawn on the screen
     #  @param floor_position the x position of the left side of the floor
     #  @exception ValueError if the floor image does not exist (NULL)
     def draw_floor(self, floor, floor_position):
-        if not floor:
-            raise ValueError("Floor image is NULL")
-        else:
-            self.__game_screen.blit(floor, (floor_position, DisplayEnvironment.Y_POS))
-            self.__game_screen.blit(floor, (floor_position + DisplayEnvironment.X_OFFSET, DisplayEnvironment.Y_POS))
+        if floor is None:
+            raise Exception("IllegalArgumentException")
+
+        self.__game_screen.blit(floor, (floor_position, DisplayEnvironment.Y_POS))
+        self.__game_screen.blit(floor, (floor_position + DisplayEnvironment.X_OFFSET, DisplayEnvironment.Y_POS))
 
 
     ## @brief Drawing the background on the game screen
@@ -68,11 +70,11 @@ class DisplayEnvironment():
     #  and blue) of the background
     #  @exception ValueError if the background image does ot exist (NULL)
     def draw_background(self, background, bg_rgb):
-        if not background:
-            raise ValueError("Background image is NULL")
-        else:
-            self.__game_screen.fill((bg_rgb[DisplayEnvironment.RED], bg_rgb[DisplayEnvironment.GREEN], bg_rgb[DisplayEnvironment.BLUE])) 
-            self.__game_screen.blit(background, DisplayEnvironment.BG_POS)
+        if background is None:
+            raise Exception("IllegalArgumentException")
+            
+        self.__game_screen.fill((bg_rgb[DisplayEnvironment.RED], bg_rgb[DisplayEnvironment.GREEN], bg_rgb[DisplayEnvironment.BLUE])) 
+        self.__game_screen.blit(background, DisplayEnvironment.BG_POS)
     
     ## @brief Drawing the powerup-time remaining onto the screen
     #  @param time The remaining time of the powerup
